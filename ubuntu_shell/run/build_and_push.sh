dd7ce522f8875c56c694b9fd8f00d80cc0b9e1ab
pascal.weiss <pascal.weiss@debtvision.de>
2025-02-10 20:22:57 +0100

    add script for building and pushing the docker image

---
+++ ubuntu_shell/run/build_and_push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+BASE_DIR=$(dirname "${0}")/..
+DOCKER_IMAGE="ubuntu-shell"
+DOCKER_TAG="latest"
+USER_NAME="user"
+
+source "${BASE_DIR}"/.env
+
+docker login "${DOCKER_REPO}" -u "${GITLAB_USER}" -p "${GITLAB_TOKEN}"
+docker build --build-arg USER_NAME="${USER_NAME}" -t "${DOCKER_IMAGE}" "${BASE_DIR}"/ubuntu-passwordless-user-tmux-ohmyzsh
+docker tag "${DOCKER_IMAGE}":"${DOCKER_TAG}" "${DOCKER_REPO}"/"${DOCKER_IMAGE}":"${DOCKER_TAG}"
+docker push "${DOCKER_REPO}"/"${DOCKER_IMAGE}":"${DOCKER_TAG}"
